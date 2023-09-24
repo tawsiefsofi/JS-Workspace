6065977808545a696008563b05022d1216e2b8a3
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2023-09-24 13:29:14 +0000

    datatypes

---
+++ BasicJS/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // By using "use strict" it will treat all JS code as newer version
+let name = "Tawsief" // String
+let age = 21 // number
+let IsLoggedIn = false // boolean 
+
+/*Premitive dataTypes
+-> number
+-> bigint
+-> string
+-> boolean
+-> symbol - unique
+-> undefined - standalone value
+-> null - value not defined
+*/
+
+console.log(typeof name) // typeof is used to check the type of a variable
+console.log(typeof null); // type of null is an object
+console.log(typeof undefined); // type of undefined is undefined
+
