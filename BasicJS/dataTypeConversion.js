164308b890e25f24d0466858a2f15c67fec7f0f9
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-01-31 14:08:13 +0000

    typeConversion

---
+++ BasicJS/dataTypeConversion.js
@@ -0,0 +1,11 @@
+//In this file we will do Data Type Conversion in JS.
+
+/*
+let score = 33;
+console.log(typeof (score)); // Output: number
+*/
+let score = "33"
+console.log(typeof (score)); // Output: string);
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber)); // Output: number
