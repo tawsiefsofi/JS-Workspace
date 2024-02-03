1be351761dfe3590bc68e0b668fafdd263416f68
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-03 18:28:17 +0000

    more about datatypes

---
+++ BasicJS/dataTypes++.js
@@ -0,0 +1,14 @@
+//Primitive data types
+// String.Number,Boolean,null,undefined,symbol,BigInt
+ 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('202121')
+const idSecond = Symbol('202121')
+console.log(id === idSecond);// Output:false because of symbol
+
