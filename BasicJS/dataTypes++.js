6ad4a8ce2c6bd6d02cd06a18397ba9bcbdfb8701
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-04 15:53:36 +0000

    Updated

--- BasicJS/dataTypes++.js
+++ BasicJS/dataTypes++.js
@@ -12,3 +12,19 @@ const id = Symbol('202121')
 const idSecond = Symbol('202121')
 console.log(id === idSecond);// Output:false because of symbol
 
+const bigNumber: 5432423535235235n
+const bigNumber = 5432423535235235n
+//Reference (Non Primitive)
+//Array Objects Functions
+
+const heros = ["Iron Man","Thor","Captian America"];
+let myObj = {
+    name:"Tawsief",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Asalamualikum");
+}
+
+console.log(typeof bigNumber);
