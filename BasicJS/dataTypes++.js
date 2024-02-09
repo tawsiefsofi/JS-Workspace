f187c7944b8b5b33d1d42fb261a77566c8ae57d3
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-09 12:03:57 +0000

    Updated

--- BasicJS/dataTypes++.js
+++ BasicJS/dataTypes++.js
@@ -1,33 +1,33 @@
 // //Primitive data types
 // // String.Number,Boolean,null,undefined,symbol,BigInt
  
-// const score = 100
-// const scoreValue = 100.3
+const score = 100
+const scoreValue = 100.3
 
-// const isLoggedIn = false
-// const outSideTemp = null
-// let userEmail;
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
 
-// const id = Symbol('202121')
-// const idSecond = Symbol('202121')
-// console.log(id === idSecond);// Output:false because of symbol
+const id = Symbol('202121')
+const idSecond = Symbol('202121')
+console.log(id === idSecond);// Output:false because of symbol
 
 // const bigNumber: 5432423535235235n
 // const bigNumber = 5432423535235235n
-// //Reference (Non Primitive)
-// //Array Objects Functions
+//Reference (Non Primitive)
+//Array Objects Functions
 
-// const heros = ["Iron Man","Thor","Captian America"];
-// let myObj = {
-//     name:"Tawsief",
-//     age:22
-// }
+const heros = ["Iron Man","Thor","Captian America"];
+let myObj = {
+    name:"Tawsief",
+    age:22
+}
 
-// const myFunction = function(){
-//     console.log("Asalamualikum");
-// }
+const myFunction = function(){
+    console.log("Asalamualikum");
+}
 
-// console.log(typeof bigNumber);
+console.log(typeof bigNumber);
 
 /* Stack (Primitive), Heap(Non-Primitive) */
 
