9016e5a899d69d1b5c0e393e6c3e3b41515e6e3b
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-09 12:31:53 +0000

    addtion to arraysd

--- BasicJS/array.js
+++ BasicJS/array.js
@@ -37,3 +37,47 @@ console.log("B ",myArr);
 const myn2 = myArr.splice(1,3)
 console.log("C ",myArr);
 console.log(myn2);
+
+//concat
+const marvelHeros = ["Hulk","Iron Man","Spiderman"]
+const dcHeros = ["Superman","Flash","Batman"]
+
+marvelHeros.push(dcHeros)
+console.log(marvelHeros); // Output: [ 'Hulk', 'Iron Man', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
+console.log(marvelHeros[3][1]); // Output: Flash
+
+const allHeros = marvelHeros.concat(dcHeros)
+console.log(allHeros); /* Output: 
+                        [
+                            'Hulk',
+                            'Iron Man',
+                            'Spiderman',
+                            [ 'Superman', 'Flash', 'Batman' ],
+                            'Superman',
+                            'Flash',
+                            'Batman'
+                        ]*/
+
+//Spread operator
+
+const allNewHeros = [...marvelHeros,...dcHeros]
+console.log(allNewHeros);
+
+//Flat method
+const arrayNew = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const arrayNewPro = arrayNew.flat(Infinity)
+console.log(arrayNewPro); // Output: [ 1, 2,3,4,5,6 7, 6, 7, 4, 5 ]
+
+//isArray ,from, of
+
+console.log(Array.isArray("Tawsief")); // Output: false
+console.log(Array.from("Tawsief")); // Output: ['T','a','w','s','i','e','f']
+
+//key value
+console.log(Array.from({name:"Tawsief"})); // Output: [] beacaue we have to mention that print this value or this key.
+
+let scoreA = 100
+let scoreB = 200
+let scoreC = 300
+
+console.log(Array.of(scoreA,scoreB,scoreC)); // Output: [ 100, 200, 300 ]
