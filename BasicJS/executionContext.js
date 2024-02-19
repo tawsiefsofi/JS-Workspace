754072aec7f4c4cf47889da25ef4b1fdcb1ce0db
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-19 17:44:28 +0000

    execution context

---
+++ BasicJS/executionContext.js
@@ -0,0 +1,15 @@
+// Execute code and Call Stack
+
+let val1= 10
+let val2 = 5
+
+function addNum(num1,num2){
+    let total = num1+num2
+    return total
+}
+
+let result1 = addNum(val1,val2)
+let result2 = addNum(5,17)
+
+console.log(result1);
+console.log(result2);
