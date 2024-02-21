107e9dd53b8d9bd424bf8c8cfa93619154959eaf
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-21 16:56:20 +0000

    switch case ,truthy and falsy

--- BasicJS/controlflow.js
+++ BasicJS/controlflow.js
@@ -16,7 +16,105 @@ else{
 
 // Nesting
 
-if(balance<500)
-{
+if (balance < 500) {
     console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+
 }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+// Basic Syntax
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// Truthy and Falsy
+
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// Falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
