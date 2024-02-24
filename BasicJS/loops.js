2dea9d59cf965ba31909843bb000ae284ceda41e
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-02-24 12:45:03 +0530

    while loop

--- BasicJS/loops.js
+++ BasicJS/loops.js
@@ -47,3 +47,26 @@ for (let index = 1; index <= 20; index++) {
    console.log(`Value of i is ${index}`);
     
 }
+
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
