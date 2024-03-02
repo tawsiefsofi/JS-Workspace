e0145bcf926c908f1c2581612e3bb7db4e439368
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-03-02 18:27:30 +0000

    edited

--- BasicJS/dom.js
+++ BasicJS/dom.js
@@ -1,35 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    
-    <title>DOM learning</title>
-    <style>
-        .bg-black{
-            background-color: #212121;
-            color: #fff;
-        }
-    </style>
-</head>
-<body class="bg-black">
-    <div >
-        <h1  id="title" class="heading">DOM learning on Chai aur code <span style="display: none;">test text</span></h1>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <input type="password" name="" id="">
 
-        <ul>
-            <li class="list-item">one</li>
-            <li class="list-item">two</li>
-            <li class="list-item">three</li>
-            <li class="list-item">four</li>
-        </ul>
-    </div>
-</body>
 
-<script>
+
+
+//DOM
+
     const parent = document.querySelector('.parent')
     // console.log(parent);
     // console.log(parent.children);
@@ -93,5 +67,3 @@ const div = document.createElement('div')
     //remove
     const lastLang = document.querySelector('li:last-child')
     lastLang.remove()
-</script>
-</html>
