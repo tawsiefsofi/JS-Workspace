6e576c50496b685f288308ba483fd05f81a0c94b
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2024-03-12 23:13:09 +0530

    Update events.js

--- BasicJS/events.js
+++ BasicJS/events.js
@@ -36,3 +36,16 @@
     
         
     })
+const sayHitesh = function(){
+        console.log("Hitesh");
+    }
+    const changeText = function(){
+        document.querySelector('h1').innerHTML = "best JS series"
+    }
+
+    const changeMe = setTimeout(changeText, 2000)
+
+    document.querySelector('#stop').addEventListener('click', function(){
+        clearTimeout(changeMe)
+        console.log("STOPPED")
+    })
